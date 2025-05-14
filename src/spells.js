08cd0f58b60e3890cea73f2bb70b9ba8e49efc10
Eric Wohnlich <eric.wohnlich@gmail.com>
2025-05-13 23:50:28 -0400

    minor visual improvement

--- src/spells.js
+++ src/spells.js
@@ -3,20 +3,20 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import evocation from "./data/spells/evocation.json";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-function Spell({name, short, spellClass}) {
+function Spell({spell}) {
+    const spellClasses = spell.classes.map(sc => <span key={sc} className="badge bg-spell-classes me-2">{sc}</span>);
     return (
         <Card style={{width: '18rem'}}>
-            <Card.Header className="bg-spell">{name}</Card.Header>
+            <Card.Header className="bg-spell text-white">{spell.name}</Card.Header>
             <Card.Body>
-                <Card.Text>
-                    {short}
+                <span className="me-1"><i className="bi bi-bandaid"></i></span>
+                {spell.short} <span className={"text-dmgtype-"+spell.dmg_type.toLowerCase()}>{spell.dmg_type}</span>
                     <ul>
-                        <li className="text-spell">Action</li>
-                        <li>{spellClass}</li>
+                        <li key="action" className="text-spell">{spell.cast_time}</li>
+                        <li key="classes">{spellClasses}</li>
                     </ul>
-                </Card.Text>
             </Card.Body>
         </Card>
     )
@@ -26,15 +26,17 @@ export default function Spells() {
     const sources = [evocation],
         spells = sources[0].map(spell => {
             return (
-                <Container>
-                    <Row>
-                        <Col>
-                        <Spell name={spell['name']} spellClass={spell['class']} short={spell['short']}/>
-                        </Col>
-                    </Row>
-                </Container>
+                <Col key={spell.name}>
+                    <Spell spell={spell}/>
+                </Col>
             )
         });
 
-    return (spells)
+    return (
+        <Container>
+            <Row>
+                {spells}
+            </Row>
+        </Container>
+    )
 }
